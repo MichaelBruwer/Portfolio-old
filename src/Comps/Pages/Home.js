80c2500569907fe2f1c21917dc1707111ae935cf
MichaelBruwer <61545579+MichaelBruwer@users.noreply.github.com>
2020-07-06 09:06:00 +0200

    re text
    removed some text

--- src/Comps/Pages/Home.js
+++ src/Comps/Pages/Home.js
@@ -6,30 +6,18 @@ const Home = () => {
   return (
     <div>
       <div className='row left-align'>
-        <div className='col s1 m5'>
-          <div className='card-content light-blue darken-3'>
-            <h4 className='green-text text-lighten-5'>
-              {' '}
-              Welcome to the landing Page
-            </h4>
-
-            <p className='green-text text-lighten-5'>
-              Enjoy this Video on Programming, Mybe you too will learn to Code
-              and be the Wizzard of Tomorow.
-            </p>
+        <div className='col s1 m10'>
+          <div class='video-container right-align'>
+            <iframe
+              title='Vid on proggraming'
+              width='853'
+              height='480'
+              src='https://youtube.com/embed/Dv7gLpW91DM'
+              frameborder='0'
+              allowfullscreen
+            ></iframe>
           </div>
         </div>
-      </div>
-
-      <div class='video-container right-align'>
-        <iframe
-          title='Vid on proggraming'
-          width='853'
-          height='480'
-          src='https://youtube.com/embed/Dv7gLpW91DM'
-          frameborder='0'
-          allowfullscreen
-        ></iframe>
       </div>
     </div>
   );
