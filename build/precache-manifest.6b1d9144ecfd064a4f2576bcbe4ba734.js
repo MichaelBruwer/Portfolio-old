be9343ef1c55eae9811df3e8fe3277e12ca45474
MichaelBruwer <61545579+MichaelBruwer@users.noreply.github.com>
2020-07-06 14:26:38 +0200

    Router updates

---
+++ build/precache-manifest.6b1d9144ecfd064a4f2576bcbe4ba734.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6cecbc3d517a4ecc5be5f1ef473b22b2",
+    "url": "/Portfolio/index.html"
+  },
+  {
+    "revision": "e91143cf84cdbe903ae9",
+    "url": "/Portfolio/static/css/2.36020323.chunk.css"
+  },
+  {
+    "revision": "504ab27d718c88045226",
+    "url": "/Portfolio/static/css/main.c70a6e32.chunk.css"
+  },
+  {
+    "revision": "e91143cf84cdbe903ae9",
+    "url": "/Portfolio/static/js/2.96b90ab3.chunk.js"
+  },
+  {
+    "revision": "7979eeaf519f92e9fad084e05c681841",
+    "url": "/Portfolio/static/js/2.96b90ab3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "504ab27d718c88045226",
+    "url": "/Portfolio/static/js/main.5430e0b3.chunk.js"
+  },
+  {
+    "revision": "b83a37198ba343383876",
+    "url": "/Portfolio/static/js/runtime-main.e440ee5a.js"
+  }
+]);
